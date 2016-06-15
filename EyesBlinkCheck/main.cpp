affa9131169f5d9a1c60e6c5d03e1ef55ac225a0
Bach Pham <pxbachs@gmail.com>
2016-06-15 16:34:19 +0700

    Added show video param

--- EyesBlinkCheck/main.cpp
+++ EyesBlinkCheck/main.cpp
@@ -6,15 +6,22 @@
 #include "frm.h"
 
 int main(int argc, char** argv) {
-	if (argc != 3) {
-		cout << "App <haar xml file> <nodered api>" << endl;
-		cout << "Ex: EyesBlinkCheck haarcascade_frontalface_alt.xml http://localhost:1880/api/eyesblinked" << endl;
+	if (argc < 3) {
+		cout << "App <haar xml file> <nodered api> " << endl;
+		cout
+				<< "Ex: EyesBlinkCheck haarcascade_frontalface_alt.xml http://localhost:1880/api/eyesblinked isShowVideo"
+				<< endl;
 		cout << "-1" << endl;
 		return -1;
 	}
 
+	bool isShowVideo = false;
+	if (argc == 4) {
+		isShowVideo = true;
+	}
+
 	frm eyesDetect;
-	eyesDetect.startWork(argv[1], argv[2]);
+	eyesDetect.startWork(argv[1], argv[2], isShowVideo);
 	waitKey(0);
 	return 0;
 }
